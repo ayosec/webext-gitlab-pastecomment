d3dc57982725dc154e8f197bce198d315a2c85bb
Ayose <ayosec@gmail.com>
2020-10-10 01:07:22 +0100

    Add "Paste" action to GitLab comments
    When the textarea element receives the focus, this extension will add a button
    with the label "Paste". On click,
    
    - It reads the content of the clipboard.
    - Replace the value of the textarea.
    - Update the preview of the comment.

---
+++ pastecomment.js
@@ -0,0 +1,59 @@
+"use strict";
+
+document.body.addEventListener("focus", event => {
+  let target = event.target;
+
+  if(target.tagName === "TEXTAREA") {
+    let write;
+    let preview;
+
+    // Find "Write" and "Preview" buttons
+
+    let form = target.closest("form");
+    if(typeof(form.dataset["__webextPasteAndReplace"]) !== "undefined") {
+      return;
+    }
+
+    form.dataset["__webextPasteAndReplace"] = true;
+
+    form.querySelectorAll("button").forEach(button => {
+      switch(button.textContent.trim()) {
+        case "Write":
+          write = button;
+          break;
+
+        case "Preview":
+          preview = button;
+          break;
+      }
+    });
+
+    // Add our own button to the toolbar
+
+    let li = document.createElement("li");
+    li.classList.add("md-header-toolbar");
+    li.style.display = "block";
+
+    preview.
+      closest("ul").
+      insertAdjacentElement("beforeend", li);
+
+    let button = document.createElement("button");
+    button.textContent = "Paste";
+    li.appendChild(button);
+
+    button.addEventListener("click", event => {
+      event.preventDefault();
+
+      // Change current tab to the editor
+      write.click();
+
+      // Get clipboard data and replace the content of the textarea
+      navigator.clipboard.readText().then(text =>  {
+        target.value = text;
+        target.dispatchEvent(new Event("change"));
+        setTimeout(() => preview.click(), 50);
+      });
+    });
+  }
+}, true);
